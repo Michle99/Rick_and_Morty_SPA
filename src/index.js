b55e0716902f6b8b9e4d79d58742bc0a17337633
Michle99 <sammiyonyx@gmail.com>
2023-10-24 18:23:27 -0400

    added custom styling

--- src/index.js
+++ src/index.js
@@ -1,11 +1,12 @@
 import ReactDOM from 'react-dom/client';
 import { List } from './components/List';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import './styles.css'
 
 function App() {
     return (
-        <div className='container'>
-            <nav className='navbar sticky-top navbar-light bg-dark'>
+        <div className='container-color container'>
+            <nav className='navbar-nav navbar navbar-expand-lg navbar-light bg-dark'>
                 <h1 className='navbar-brand text-light'>
                     Rick and Morty
                 </h1>
